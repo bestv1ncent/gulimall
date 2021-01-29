0a618eaa29bc74384ef7f1635bd685a30e60d328
bestv1ncent <983874606@qq.com>
2021-01-30 01:56:34 +0800

    项目初始结构创建

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,27 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ *1.整合Mybatis-plus
+ * <dependency>
+ *     <groupId>com.baomidou</groupId>
+ *     <artifactId>mybatis-plus-boot-starter</artifactId>
+ *     <version>3.2.0</version>
+ * </dependency>
+ *
+ * 2.配置
+ *      1.配置数据源
+ *          1)、导入数据库驱动
+ *          2)、在application.yml 中配置相关信息
+ *      2.配置Mybatis-Plus
+ *          1)、使用@Mapper-Scan
+ *          2)、告诉Mybatis-Plus.SQl 映射文件位置
+*/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallProductApplication {
 
